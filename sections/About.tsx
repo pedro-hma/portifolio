6fa065eb4802148aacaee58adc7607dcd102c0f8
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2026-02-27 20:16:55 +0000

    mais detalhes pessoais

--- sections/About.tsx
+++ sections/About.tsx
@@ -3,7 +3,14 @@ export default function About() {
     <section id="sobre" style={{ padding: 20 }}>
       <h2>Sobre mim</h2>
       <p>
-        Desenvolvedor focado em criar interfaces modernas e performáticas.
+        Desenvolvedor focado em criar interfaces modernas e desenvolver sistemas web mobile.
+        Forte interesse em tecnologia, programação e criação de soluções digitais.
+        Atualmente, tenho aplicado meus conhecimentos atraves de projetos praticos, explorando desde conceitos fundamentais até desevolvimento de aplicações web
+      </p>
+      <p>
+        Tenho experiência com <strong>Java, Programação Orientada a Objetos,
+    Estruturas de Dados</strong> e desenvolvimento web utilizando
+    <strong> React, Next.js e TypeScript</strong>.
       </p>
     </section>
   )
